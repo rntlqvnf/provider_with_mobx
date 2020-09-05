c7f17d030886061792f1a8d30238afd4aaf5762e
hajae <rntlqvnf@naver.com>
2020-09-05 14:49:53 +0900

    <Feat> : Implement MobxProvider
    MobxProvider gives you a simple store provider.

--- lib/src/mobx_provider.dart
+++ lib/src/mobx_provider.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:mobx/mobx.dart';
+import 'package:provider/provider.dart';
 
+class MobxProvider<T extends Store> extends StatelessWidget {
+  final Widget Function(T store, BuildContext context) builder;
+  final T Function(BuildContext context) create;
+  final T Function(BuildContext context, T store) dispose;
+  final bool lazy;
+
+  const MobxProvider(
+      {Key key, this.builder, this.create, this.dispose, this.lazy})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Provider<T>(
+      create: create,
+      dispose: dispose,
+      lazy: lazy,
+      child: Builder(builder: (context) => builder(context.read<T>(), context)),
+    );
+  }
+}
